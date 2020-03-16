5fb0eb3d2e2cc61c4b5e596cacedd47a32fa601c
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-03-16 16:50:31 -0500

    finish first part of exercise

---
+++ js/external.js
@@ -0,0 +1,21 @@
+console.log("Hello from external JavaScript");
+
+var name = "Sarah";
+console.log(name);
+
+alert("Welcome to my Website!");
+
+var input = prompt("What is your favorite color?");
+console.log("Great, " + input +" is my favorite color too!");
+
+
+
+
+
+	// console.log("Hello from inline JavaScript")
+
+
+	// 	alert("This is an alert!");
+
+	// 	var confirmed = confirm('Are you sure you want to delete all files?');
+	// 	console.log("confirm " + confirmed);
