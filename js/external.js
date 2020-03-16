f8ce3d668e09b2b396f0448481a78ee55e11f190
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-03-16 17:37:42 -0500

    finish Javascript with HTML exercise

--- js/external.js
+++ js/external.js
@@ -9,13 +9,35 @@ var input = prompt("What is your favorite color?");
 console.log("Great, " + input +" is my favorite color too!");
 
 
+// Exercise 3 #1
+var LittleMermaid = 3;
+var BrotherBear = 5;
+var Hercules = 1;
 
+alert("Answer to Exercise 3 #1: " + 3 * (LittleMermaid + BrotherBear + Hercules));
 
+// Exercise 3 #2
+var Google = 400;
+var Amazon = 380;
+var Facebook = 350;
 
-	// console.log("Hello from inline JavaScript")
+alert("Answer to Exercise 3 #2: " + ((10 * Facebook) + (6 * Google) + (4 * Amazon)));
 
+// Exercise 3 #3
+var classNotFull = true;
+var noScheduleConflict = true;
+var canBeEnrolled = classNotFull && noScheduleConflict;
 
-	// 	alert("This is an alert!");
+console.log(classNotFull && !noScheduleConflict);
 
-	// 	var confirmed = confirm('Are you sure you want to delete all files?');
-	// 	console.log("confirm " + confirmed);
+console.log(canBeEnrolled);
+
+// Exercise 3 #4
+var ItemsBought = 2;
+var OfferNotExpired = true;
+var isPremiumMember = true;
+var isOfferValid = (isPremiumMember && OfferNotExpired) || (ItemsBought >= 2 && OfferNotExpired);
+
+console.log(!isPremiumMember && OfferNotExpired);
+console.log(isOfferValid);
+
