07fbc677dfcdf501e81e4a48b50092e37a14f47e
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-03-17 11:41:11 -0500

    edited external js exercise after walkthrough

--- js/external.js
+++ js/external.js
@@ -1,36 +1,39 @@
 console.log("Hello from external JavaScript");
 
-var name = "Sarah";
-console.log(name);
+// var name = "Sarah";
+// console.log(name);
 
 alert("Welcome to my Website!");
 
 var input = prompt("What is your favorite color?");
-console.log("Great, " + input +" is my favorite color too!");
+alert("Great, " + input +" is my favorite color too!");
 
 
 // Exercise 3 #1
-var LittleMermaid = 3;
-var BrotherBear = 5;
-var Hercules = 1;
+var LittleMermaid = prompt("how many days do you want to rent The Little Mermaid?");
+var BrotherBear = prompt("how many days do you want to rent Brother Bear?");
+var Hercules = prompt("how many days do you want to rent Hercules?");
 
-alert("Answer to Exercise 3 #1: " + 3 * (LittleMermaid + BrotherBear + Hercules));
+var TotalCost = (parseInt(LittleMermaid) + parseInt(BrotherBear) + parseInt(Hercules)) * 3;
+alert("Answer to Exercise 3 #1: Total Cost = $" + TotalCost);
 
 // Exercise 3 #2
-var Google = 400;
-var Amazon = 380;
-var Facebook = 350;
+var Google = 400, Amazon = 380, Facebook = 350;
 
-alert("Answer to Exercise 3 #2: " + ((10 * Facebook) + (6 * Google) + (4 * Amazon)));
+var TotalPay = ((10 * Facebook) + (6 * Google) + (4 * Amazon));
+
+alert("Answer to Exercise 3 #2: Total Pay = $" + TotalPay);
 
 // Exercise 3 #3
 var classNotFull = true;
 var noScheduleConflict = true;
 var canBeEnrolled = classNotFull && noScheduleConflict;
 
-console.log(classNotFull && !noScheduleConflict);
+// console.log(classNotFull && !noScheduleConflict);
 
 console.log(canBeEnrolled);
+alert("Student can attend class " + canBeEnrolled);
+
 
 // Exercise 3 #4
 var ItemsBought = 2;
