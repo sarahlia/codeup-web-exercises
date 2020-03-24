609b1ef12e79713b3761d5e37c6333ad7da4e8d4
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-03-24 15:12:45 -0500

    finish for while and for loops exercises

---
+++ js/for_loops.js
@@ -0,0 +1,52 @@
+"use strict";
+
+// Exercise 2
+for(var i = 1; i <= 10; i++) {
+    function showMultiplicationTable(numb) {
+        return numb * i;
+    }
+    console.log("7 X " + i + " = " + showMultiplicationTable(7));
+}
+
+// Exercise 3
+var randomNumber = Math.floor(Math.random() * 200) + 20;
+console.log(randomNumber);
+
+for(var i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(randomNumber + i + " is even");
+    } else {
+        console.log(randomNumber + i + " is odd.");
+    }
+}
+// var na = '2';
+// console.log(na.repeat(2));
+
+// Exercise 4
+for(var i = 1; i <= 9; i++) {
+    console.log(i.toString().repeat(i));
+    // if (i === 2) {
+    //     console.log("2".repeat(i));
+    // } else if (i === 3) {
+    //     console.log(i + "33");
+    // } else if (i === 4) {
+    //     console.log(i + "444");
+    // } else if (i === 5) {
+    //     console.log(i + "5555");
+    // } else if (i === 6) {
+    //     console.log(i + "66666");
+    // } else if (i === 7) {
+    //     console.log(i + "777777");
+    // } else if (i === 8) {
+    //     console.log(i + "8888888");
+    // } else if (i === 9) {
+    //     console.log(i + "99999999");
+    // } else {
+    //     console.log("1");
+    // }
+}
+
+// Exercise 5
+for(var i = 100; i >= 5; i-=5) {
+    console.log(i);
+}
