38123cae097145430d066d9ab05f75e7343cf963
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-03-25 13:59:53 -0500

    edit break and continue exercise after walkthrough

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,27 +1,45 @@
 "use strict";
 
-// var aNumb =  Math.floor((Math.random()*50)+1);
-// console.log("Number to skip is: " + aNumb);
+// var input = prompt("Enter an odd number between 1 and 50.");
+// console.log("Number to skip: " + parseInt(input));
+//
+//     for(var n = 1; n <= 50; n+=2) {
+//         if (parseInt(input) % 2 === 0) {
+//             alert("Refresh the page and enter a valid number.");
+//             break;
+//
+//         } else if (n === parseInt(input)) {
+//             console.log("Yikes! Skipping number: " + n);
+//             continue;
+//         }
+//         console.log("Here is an odd number: " + n);
+//     }
 
-var input = prompt("Enter an odd number between 1 and 50.");
-console.log("Number to skip: " + parseInt(input));
 
-    for(var i = 1; i <= 50; i+=2) {
-        if (parseInt(input) % 2 === 0) {
-            alert("Refresh the page and enter a valid number.");
-            break;
-
-                // if (parseInt(inp2) % 2 !== 0) {
-                //     if (i === parseInt(inp2)) {
-                //         console.log("Yikes! Skipping number: " + i);
-                //     } else {
-                //         console.log("Here is an odd number: " + i);
-                //     }
-                // }
-
-        } else if (i === parseInt(input)) {
-            console.log("Yikes! Skipping number: " + i);
-            continue;
-        }
-        console.log("Here is an odd number: " + i);
+var numberInp = 0;
+do {
+     numberInp = prompt("Enter an odd number between 1 and 50.");
+    if (numberInp < 1|| numberInp > 50) {
+        alert(numberInp + " is not between 1 and 50. Please enter a valid number.");
+    } else if (numberInp % 2 === 0) {
+        alert(numberInp + " is not odd. Please enter a valid number.");
+    } else if (isNaN(numberInp)) {
+        alert(numberInp + " is not a number. Please enter a valid number.")
+    } else {
+        alert("Thanks. You entered a valid number.");
+        break;
     }
+} while (true);
+
+
+console.log('Number to skip is: ' + numberInp);
+
+for(var i = 1; i <= 50; i+=2) {
+    if (i === parseInt(numberInp)) {
+        console.log("Yikes! Skipping number: " + i);
+        continue;
+    }
+    console.log("Here is an odd number: " + i);
+}
+
+
