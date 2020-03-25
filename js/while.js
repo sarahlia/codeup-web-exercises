8afe53e35fda8f7e0c5c3a34def109cd742cda9d
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-03-25 11:39:03 -0500

    edit while exercise after walkthrough

--- js/while.js
+++ js/while.js
@@ -11,31 +11,22 @@ while(i <= 16) {
 // Write a JS program that generates a random number between 50 and 100 representing the amount of cones to sell.
 // Your code should generate numbers between 1 and 5, simulating the amount of cones being bought by her clients.
 // Use a do-while loop to log to the console the amount of cones sold to each person.
-// This is how you get the random numbers.
 
-// This is how you get a random number between 50 and 100
+
 var allCones = Math.floor(Math.random() * 50) + 50;
 console.log("Number of cones for sale: " + allCones);
-// This expression will generate a random number between 1 and 5
-var conesBought = Math.floor(Math.random() * 5) + 1;
-console.log("Number of cones bought: " + conesBought);
-
-var i = 1;
 
 do {
-    if (conesBought < allCones) {
-        console.log(conesBought + " cones sold...");
-        i++;
-    } else if (conesBought > allCones) {
-        console.log("Cannot sell you " + conesBought + "I only have " + allCones);
-        i++;
+    var conesToBuy = Math.floor(Math.random() * 5) + 1;
+
+    if (conesToBuy <= allCones) {
+        console.log(conesToBuy + " cones sold...");
+        //allCones = allCones - conesToBuy
+        allCones -= conesToBuy;
     } else {
-        console.log("Yay! I sold them all!");
+        console.log("Cannot sell you " + conesToBuy + " cones, I only have " + allCones);
     }
+    console.log(allCones);
+} while (allCones > 0);
+console.log("Yay! I sold them all!");
 
-} while (i < conesBought);
-
-// The output should be similar to the following:
-//5 cones sold...  // if there are enough cones
-//Cannot sell you 6 cones I only have 3...  // If there are not enough cones
-//Yay! I sold them all! // If there are no more cones
