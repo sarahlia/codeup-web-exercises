40fd74278e30cc3c4930ea0bbdc0872c02f05323
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-04-04 00:10:01 -0500

    edited forEach loop and finished maps api exercise

--- js/mapbox-maps-api.js
+++ js/mapbox-maps-api.js
@@ -1,33 +1,52 @@
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v9',
-    zoom: 15
+    zoom: 0
 });
 
-geocode("6602 Melrose Ave, Los Angeles, CA 90038", mapboxToken2).then(function(result) {
-    console.log(result);
-    map.setCenter(result);
+var restaurants = [
+    {
+        address: "6602 Melrose Ave, Los Angeles, CA 90038",
+        info: "<strong>Osteria Mozza</strong><br>Italian restaurant<br>Los Angeles, CA, United States"
 
-    // map.jumpTo({center: result});
-    // map.flyTo({center: result});
+    },
+    {
+        address: "Viale Pasitea, 236, 84017 Positano SA, Italy",
+        info: "<strong>Mediterraneo</strong><br>Mediterranean cuisine<br>Positano, Italy"
+
+    },
+    {
+        address: "Jl. Teuku Umar No.1, RT.1/RW.1, Gondangdia, Kec. Menteng, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta 10350, Indonesia",
+        info: "<strong>Kunstkring</strong><br>Indonesian-Dutch restaurant<br>Jakarta, Indonesia"
+
+    }
+];
+
+restaurants.forEach(function(aResto, index) {
+    geocode(aResto.address, mapboxToken2).then(function(coordinates) {
+
+
+        var marker = new mapboxgl.Marker()
+            .setLngLat(coordinates)
+            // .setPopup(popup)
+            .addTo(map);
+
+        var popup = new mapboxgl.Popup()
+            .setHTML(aResto.info);
+            // .setText(aResto.address);
+        // .setDOMContent(div)
+        // .addTo(map);
+
+        marker.setPopup(popup);
+    });
+
+    // to set Zoom in the JS Console:
+    // map.setZoom(n);
+
+
 });
 
-var markerOptions = {
-    color: "red"
-};
 
-var div = window.document.createElement('div');
-div.innerHTML = 'Osteria Mozza';
 
-var popup = new mapboxgl.Popup()
-    // .setText("Osteria Mozza")
-    .setDOMContent(div)
-    .addTo(map);
 
-var marker = new mapboxgl.Marker(markerOptions)
-    .setLngLat([-118.338942, 34.083393])
-    .setPopup(popup)
-    .addTo(map);
-
-// marker.setPopup(popup);
 
