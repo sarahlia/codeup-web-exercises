771cc32add32cbcc3d980eb51f018fa00991577a
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-04-16 15:22:18 -0500

    able to return lat and long coordinates on marker dragend

--- js/map-weather-project.js
+++ js/map-weather-project.js
@@ -1,15 +1,28 @@
-mapboxgl.accessToken = 'pk.eyJ1Ijoic2FyYWhsaWEiLCJhIjoiY2s4ajhiMndkMGU3ODNrcW1jeDRocjhtMiJ9.llxRsa9CU8ZUq0qW-pXIlg';
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9',
-    zoom: 12,
-    center: [-96.7297, 32.9482]
+"use strict";
+$(document).ready(function() {
+
+    mapboxgl.accessToken = 'pk.eyJ1Ijoic2FyYWhsaWEiLCJhIjoiY2s4ajhiMndkMGU3ODNrcW1jeDRocjhtMiJ9.llxRsa9CU8ZUq0qW-pXIlg';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9',
+        zoom: 12,
+        center: [-96.7297, 32.9482],
+        draggable: true,
+        // maxZoom: 5
+    });
+
+    var coordinates = $('#coordinates');
+
+    var marker = new mapboxgl.Marker({
+        draggable: true
+    })
+        .setLngLat([-96.7297, 32.9482])
+        .addTo(map);
+
+    function onDragEnd() {
+        var lngLat = marker.getLngLat();
+        coordinates.css('visibility', 'visible');
+        coordinates.html('Longitude: ' + lngLat.lng + '<br>Latitude: ' + lngLat.lat);
+    }
+    marker.on('dragend', onDragEnd);
 });
-
-
-// var map = new mapboxgl.Map({
-//     container: 'map',
-//     style: 'mapbox://styles/mapbox/streets-v9',
-//     zoom: 10
-// });
-
