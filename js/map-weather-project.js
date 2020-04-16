75f4d1954d263538f5d3278f148e323a3e649a54
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-04-16 15:36:48 -0500

    place coordinates in the right location below the map

--- js/map-weather-project.js
+++ js/map-weather-project.js
@@ -1,14 +1,13 @@
 "use strict";
 $(document).ready(function() {
 
-    mapboxgl.accessToken = 'pk.eyJ1Ijoic2FyYWhsaWEiLCJhIjoiY2s4ajhiMndkMGU3ODNrcW1jeDRocjhtMiJ9.llxRsa9CU8ZUq0qW-pXIlg';
+    mapboxgl.accessToken = mapboxToken2
     var map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v9',
         zoom: 12,
         center: [-96.7297, 32.9482],
         draggable: true,
-        // maxZoom: 5
     });
 
     var coordinates = $('#coordinates');
