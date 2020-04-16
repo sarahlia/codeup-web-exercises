db19aafec6968ca9dd2c25fc787f196847aec311
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-04-16 12:24:12 -0500

    got map in place

---
+++ js/map-weather-project.js
@@ -0,0 +1,15 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2FyYWhsaWEiLCJhIjoiY2s4ajhiMndkMGU3ODNrcW1jeDRocjhtMiJ9.llxRsa9CU8ZUq0qW-pXIlg';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    zoom: 12,
+    center: [-96.7297, 32.9482]
+});
+
+
+// var map = new mapboxgl.Map({
+//     container: 'map',
+//     style: 'mapbox://styles/mapbox/streets-v9',
+//     zoom: 10
+// });
+
