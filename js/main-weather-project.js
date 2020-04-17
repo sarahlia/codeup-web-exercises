f506a6a1abb4c4e6e05b494af5a94617e0677c2e
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-04-16 22:55:13 -0500

    generated dynamic weather cards on marker dragend

--- js/main-weather-project.js
+++ js/main-weather-project.js
@@ -1,58 +1,55 @@
 "use strict";
 (function() {
     $(document).ready(function() {
-        //ajax--OpenWeather API
         var restHTML = '';
-
+        //Ajax request to OpenWeather for current city
         $.get("http://api.openweathermap.org/data/2.5/forecast", {
             "APPID": OWM_KEY,
             "q": "Richardson, US",
             "units": "imperial"
         }).done(function(data) {
             console.log(data);
-
             $('#forecast').empty();
+            currentCityForecastHTML();
+            //weather cards/5-day forecast for current city
+            function currentCityForecastHTML(city) {
+                for (var i = 0; i < 40; i+=8) {
+                    var date = data.list[i].dt_txt;
+                    var temp = data.list[i].main.temp;
+                    console.log(temp);
+                    var description = data.list[i].weather[0].description;
+                    console.log(description);
+                    var humidity = data.list[i].main.humidity;
+                    console.log(humidity);
+                    var wind = data.list[i].wind.speed;
+                    console.log(wind);
+                    var pressure = data.list[i].main.pressure;
+                    console.log(pressure);
+                    var weatherIcon = data.list[i].weather[0].icon;
+                    console.log(weatherIcon);
+                    var iconImage = 'http://openweathermap.org/img/w/' + weatherIcon + '.png';
+                    console.log(iconImage);
 
-            //weather info for each card
-            for (var i = 0; i < 40; i+=8) {
-                var date = data.list[i].dt_txt;
-                var temp = data.list[i].main.temp;
-                console.log(temp);
-                var description = data.list[i].weather[0].description;
-                console.log(description);
-                var humidity = data.list[i].main.humidity;
-                console.log(humidity);
-                var wind = data.list[i].wind.speed;
-                console.log(wind);
-                var pressure = data.list[i].main.pressure;
-                console.log(pressure);
+                    restHTML = '<div class="col card" style="width: 18rem;">';
+                    restHTML += '<div class="card-header">';
+                    restHTML += date;
+                    restHTML += '</div>';
+                    restHTML += '<ul class="list-group list-group-flush"></ul>';
+                    restHTML += '<li class="list-group-item">' + temp +'°F<span><img alt="weather icon" src=' + iconImage + '></span></li>';
+                    // restHTML += '<img alt="weather icon" src=' + iconImage + '>';
+                    restHTML += '<li class="list-group-item">' + description + '</li>';
+                    restHTML += '<li class="list-group-item">Humidity: ' + humidity +'</li>';
+                    restHTML += '<li class="list-group-item">Wind: ' + wind +'</li>';
+                    restHTML += '<li class="list-group-item">Pressure: ' + pressure +'</li>';
+                    restHTML += '</div>'
 
-                var weatherIcon = data.list[i].weather[0].icon;
-                console.log(weatherIcon);
-                var iconImage = 'http://openweathermap.org/img/w/' + weatherIcon + '.png';
-                console.log(iconImage);
-
-                restHTML = '<div class="col card" style="width: 18rem;">';
-                restHTML += '<div class="card-header">';
-                restHTML += date;
-                restHTML += '</div>';
-                restHTML += '<ul class="list-group list-group-flush"></ul>';
-                restHTML += '<li class="list-group-item">' + temp +'°F<span><img alt="weather icon" src=' + iconImage + '></span></li>';
-                // restHTML += '<img alt="weather icon" src=' + iconImage + '>';
-                restHTML += '<li class="list-group-item">' + description + '</li>';
-                restHTML += '<li class="list-group-item">Humidity: ' + humidity +'</li>';
-                restHTML += '<li class="list-group-item">Wind: ' + wind +'</li>';
-                restHTML += '<li class="list-group-item">Pressure: ' + pressure +'</li>';
-                restHTML += '</div>'
-
-                $('#forecast').append(restHTML);
-
+                    $('#forecast').append(restHTML);
+                }
             }
 
         }).fail(function(errors) {
             console.error(errors);
         });
-
 
         //Mapbox API -- map
         mapboxgl.accessToken = mapboxToken2
@@ -72,18 +69,62 @@
             .addTo(map);
 
         var coordinates = $('#coordinates');
-        //add event handler (get coordinates) at the end of marker drag
+        var Long;
+        var Lat;
+        //get coordinates when marker is moved
         function onDragEnd() {
             var lngLat = marker.getLngLat();
+            var Long = lngLat.lng;
+            var Lat = lngLat.lat;
             coordinates.css('visibility', 'visible');
             coordinates.html('Longitude: ' + lngLat.lng + '<br>Latitude: ' + lngLat.lat);
-        }
-        marker.on('dragend', onDragEnd);
+            console.log(Long);
+            console.log(Lat);
+                // feed coordinates from new marker/city to generate new weather cards/5-day forecast:
+                $.get("http://api.openweathermap.org/data/2.5/forecast", {
+                    "APPID": OWM_KEY,
+                    "lat": Lat,
+                    "lon": Long,
+                    "units": "imperial"
+                }).done(function(data) {
+                    $('#forecast').empty();
+                    for (var i = 0; i < 40; i+=8) {
+                        var date = data.list[i].dt_txt;
+                        var temp = data.list[i].main.temp;
+                        console.log(temp);
+                        var description = data.list[i].weather[0].description;
+                        console.log(description);
+                        var humidity = data.list[i].main.humidity;
+                        console.log(humidity);
+                        var wind = data.list[i].wind.speed;
+                        console.log(wind);
+                        var pressure = data.list[i].main.pressure;
+                        console.log(pressure);
+                        var weatherIcon = data.list[i].weather[0].icon;
+                        console.log(weatherIcon);
+                        var iconImage = 'http://openweathermap.org/img/w/' + weatherIcon + '.png';
+                        console.log(iconImage);
 
-        var droppedLong = marker.getLngLat().lng;
-        console.log(droppedLong);
-        var droppedLat = marker.getLngLat().lat;
-        console.log(droppedLat);
+                        restHTML = '<div class="col card" style="width: 18rem;">';
+                        restHTML += '<div class="card-header">';
+                        restHTML += date;
+                        restHTML += '</div>';
+                        restHTML += '<ul class="list-group list-group-flush"></ul>';
+                        restHTML += '<li class="list-group-item">' + temp +'°F<span><img alt="weather icon" src=' + iconImage + '></span></li>';
+                        restHTML += '<li class="list-group-item">' + description + '</li>';
+                        restHTML += '<li class="list-group-item">Humidity: ' + humidity +'</li>';
+                        restHTML += '<li class="list-group-item">Wind: ' + wind +'</li>';
+                        restHTML += '<li class="list-group-item">Pressure: ' + pressure +'</li>';
+                        restHTML += '</div>'
+
+                        $('#forecast').append(restHTML);
+                    }
+                }).fail(function(errors) {
+                    console.error(errors);
+                });
+        }
+        //add event handler at the end of marker drag -- get coordinates, then feed those coordinates to generate new cards
+        marker.on('dragend', onDragEnd);
 
     });
 
