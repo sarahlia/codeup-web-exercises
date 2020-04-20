03d6a6a27863fc641920344b68358ab7b8d2fbd7
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-04-19 20:52:01 -0500

    working on scratch exercise

---
+++ home-practice/scratch.js
@@ -0,0 +1,46 @@
+var cars = [
+    {
+        make: "Toyota",
+        model: "Camry",
+        features: ["Automatic Windows", "Bluetooth Connectivity", "GPS Navigation"],
+        owner: {
+            name: "Jane Doe",
+            age: 30
+        }
+    },
+    {
+        make: "Honda",
+        model: "Accord",
+        features: ["Automatic Windows", "Great Gas Mileage", "AM/FM Radio"],
+        owner: {
+            name: "John Doe",
+            age: 31
+        }
+    }
+];
+
+console.log("The first car is a " + cars[0].make + " " + cars[0].model + ".");
+console.log("The owner of the second car is " + cars[1].owner.name + ".");
+
+console.log("Here are all the features of all the cars:");
+cars.forEach(function(car) {
+    car.features.forEach(function(feature) {
+        console.log(feature);
+    });
+});
+
+
+"use strict";
+
+var car = {};
+car.make = "Toyota";
+car.model = "Camry";
+
+// create a logMakeModel method on the car object
+car.logMakeModel = function () {
+    console.log("Car make/model is: " + this.make + " " + this.model);
+};
+
+// log the make/model
+car.logMakeModel();
+
