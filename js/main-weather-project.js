370a9973e8c320277d8a3692e40bf669916fcb0d
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-04-22 00:23:17 -0500

    included both min and max temperatures, converted temp to integer

--- js/main-weather-project.js
+++ js/main-weather-project.js
@@ -2,43 +2,45 @@
 (function() {
     $(document).ready(function() {
         var restHTML = '';
-        //Ajax request to OpenWeather
+        //Ajax request to OpenWeather - Richardson
         $.get("http://api.openweathermap.org/data/2.5/forecast", {
             "APPID": OWM_KEY,
             "q": "Richardson, US",
             "units": "imperial"
         }).done(updateWeather)
-            .fail(function(errors) {
-                console.error(errors);
-            });
+        .fail(function(errors) {
+            console.error(errors);
+        });
 
         //generate weather cards/5-day forecast
         function updateWeather(data) {
             $('#forecast').empty();
             for (var i = 0; i < 40; i+=8) {
                 var date = data.list[i].dt_txt;
-                var temp = data.list[i].main.temp;
-                console.log(temp);
+                var tempMin = data.list[i].main.temp_min;
+                tempMin = parseInt(tempMin);
+                // console.log(tempMin);
+                var tempMax = data.list[i].main.temp_max;
+                tempMax = parseInt(tempMax);
                 var description = data.list[i].weather[0].description;
-                console.log(description);
+                // console.log(description);
                 var humidity = data.list[i].main.humidity;
-                console.log(humidity);
+                // console.log(humidity);
                 var wind = data.list[i].wind.speed;
-                console.log(wind);
+                // console.log(wind);
                 var pressure = data.list[i].main.pressure;
-                console.log(pressure);
+                // console.log(pressure);
                 var weatherIcon = data.list[i].weather[0].icon;
-                console.log(weatherIcon);
+                // console.log(weatherIcon);
                 var iconImage = 'http://openweathermap.org/img/w/' + weatherIcon + '.png';
-                console.log(iconImage);
+                // console.log(iconImage);
 
                 restHTML = '<div class="col card" style="width: 18rem;">';
                 restHTML += '<div class="card-header">';
                 restHTML += date;
                 restHTML += '</div>';
                 restHTML += '<ul class="list-group list-group-flush"></ul>';
-                restHTML += '<li class="list-group-item">' + temp +'°F<span><img alt="weather icon" src=' + iconImage + '></span></li>';
-                // restHTML += '<img alt="weather icon" src=' + iconImage + '>';
+                restHTML += '<li class="list-group-item">' + tempMin + '/' + tempMax +'°F<span><img alt="weather icon" src=' + iconImage + '></span></li>';
                 restHTML += '<li class="list-group-item">' + description + '</li>';
                 restHTML += '<li class="list-group-item">Humidity: ' + humidity +'</li>';
                 restHTML += '<li class="list-group-item">Wind: ' + wind +'</li>';
@@ -75,9 +77,9 @@
             var Long = lngLat.lng;
             var Lat = lngLat.lat;
             coordinates.css('visibility', 'visible');
-            coordinates.html('Longitude: ' + lngLat.lng + '<br>Latitude: ' + lngLat.lat);
-            console.log(Long);
-            console.log(Lat);
+            coordinates.html('Longitude: ' + Long + '<br>Latitude: ' + Lat);
+            // console.log(Long);
+            // console.log(Lat);
             // feed coordinates from new marker to generate new weather cards:
             $.get("http://api.openweathermap.org/data/2.5/forecast", {
                 "APPID": OWM_KEY,
@@ -85,9 +87,9 @@
                 "lon": Long,
                 "units": "imperial"
             }).done(updateWeather)
-                .fail(function(errors) {
-                    console.error(errors);
-                });
+            .fail(function(errors) {
+                console.error(errors);
+            });
         }
         //add event handler at the end of marker drag -- execute function onDragEnd
         marker.on('dragend', onDragEnd);
@@ -107,15 +109,15 @@
         // update the weather cards when a new location is entered
         searchBox.keyup(function () {
             var typedInput = searchBox[0].value;
-            console.log(typedInput);
+            // console.log(typedInput);
             $.get("http://api.openweathermap.org/data/2.5/forecast", {
                 "APPID": OWM_KEY,
                 "q": typedInput,
                 "units": "imperial"
             }).done(updateWeather)
-                .fail(function(errors) {
-                    console.error(errors);
-                });
+            .fail(function(errors) {
+                console.error(errors);
+            });
 
             if (typedInput !== "") {
                 marker.remove();
