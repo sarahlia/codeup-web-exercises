26cbe36f018c3fa5ba124b3650ff1a5ae12701bf
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-04-22 17:28:59 -0500

    included a note with the for-of method for the first exercise

--- js/promises.js
+++ js/promises.js
@@ -6,27 +6,33 @@
                 .then( response => response.json()
                     .then( data => data[0].created_at )
                 )
-                    // let lastCommit = data[0].created_at
-                    // console.log(lastCommit);
-                    // return lastCommit;
-                //     })
-                // ).catch( error => console.error(error) );
         }
         getDate('sarahlia').then(lastCommit => console.log("last commit date: " + lastCommit));
 
-function wait(num) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (typeof num === "number") {
-                resolve(num);
-            // } else {
-            //     reject('Network Connection Error!');
-            }
-        }, num);
-    });
-}
 
-const request = wait(2000);
-console.log(request); // pending promise
-request.then(num => console.log("You'll see this after " + num + " milliseconds."));
-request.catch(error => console.log('Promise rejected!', error));
+// if we only want the PushEvent of the last commit date, do a for-of loop:
+// function getDate(username) {
+//     let url = `https://api.github.com/users/${username}/events/public`;
+//     return fetch(url, {headers: {'Authorization': GITHUB_KEY}})
+//         .then( response => response.json()
+//             .then( listOfEvents => {
+//                 for(let event of listOfEvents) {
+//                     console.log(event);
+//                     if(event.type === 'PushEvent') {
+//                         return event.created_at;
+//                     }
+//                 }
+//             })
+//         )
+// }
+// getDate('sarahlia').then(lastCommit => console.log("last commit date: " + lastCommit));
+
+    function wait(num) {
+        return new Promise((resolve, reject) => {
+            setTimeout( () => resolve(num)
+            ,num);
+        });
+    }
+    wait(2000);
+    console.log(wait(2000));
+    wait(2000).then( (ms) => console.log(`You'll see this after ${ms/1000} seconds.`));
