701a8a9e6568bff7db5f18d3bd27c3f69b2c9ee1
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-04-23 12:01:28 -0500

    finish all 6 exercises

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -41,9 +41,9 @@ let proficient = users.filter( person => person.languages.length >= 3 );
 console.log(proficient);
 
 //Exercise 3: use .map to create an array of strings where each element is a user's email address
-let userEmail = users.map( person => person.email);
-console.log(userEmail);
-console.log(userEmail.length);
+let userEmails = users.map( person => person.email);
+console.log(userEmails);
+console.log(userEmails.length);
 
 
 //Exercise 4a: use .reduce to get the total years of experience from the list of users
@@ -57,7 +57,7 @@ let averageYearsExp = totalYearsExp/(users.length);
 console.log(averageYearsExp);
 
 //Exercise 5: use .reduce to get the longest email from the list of users
-let longestEmail = userEmail.reduce ( (acc, unit) => {
+let longestEmail = userEmails.reduce ( (acc, unit) => {
     return  acc.length > unit.length ? acc : unit;
     // if (acc.length > unit.length) {
     //     return acc;
@@ -68,5 +68,13 @@ let longestEmail = userEmail.reduce ( (acc, unit) => {
 }, "");
 console.log(longestEmail);
 
+//Exercise 6: use .reduce to get the list of user's names in a single string:
+let userNames = users.map( person => person.name ) ;
+console.log(userNames);
+
+let namesString = userNames.reduce ( (acc, unit) => {
+    return acc + unit;
+}, "");
+console.log(namesString);
 
 
