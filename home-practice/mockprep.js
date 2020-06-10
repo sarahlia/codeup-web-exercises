4bde1a155d4ad536110b4ffe86664f1ead1d390e
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-06-09 21:29:54 -0500

    finish writing a string reversal function

--- home-practice/mockprep.js
+++ home-practice/mockprep.js
@@ -1,16 +1,23 @@
-// function fizzBuzz() {
-    for(let i=1; i<=100; i++) {
-        if(i % 15 == 0) {
-            console.log("FizzBuzz");
-        } else if(i % 3 == 0) {
-            console.log("Fizz");
-        } else if(i % 5 == 0) {
-            console.log("Buzz");
-        } else {
-            console.log(i);
-        }
+// FizzBuzz challenge using Javascript
+//     for(let i=1; i<=100; i++) {
+//         if(i % 15 == 0) {
+//             console.log("FizzBuzz");
+//         } else if(i % 3 == 0) {
+//             console.log("Fizz");
+//         } else if(i % 5 == 0) {
+//             console.log("Buzz");
+//         } else {
+//             console.log(i);
+//         }
+//
+//     }
 
+// Write a string reversal function
+    function reverseString(str) {
+        let splitStr = str.split("");
+        let reverseArr = splitStr.reverse();
+        let joinArr = reverseArr.join("");
+        return joinArr;
     }
-// }
 
-// fizzBuzz();
+    reverseString("Hello World");
